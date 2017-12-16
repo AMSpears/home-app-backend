16aaaf31b81c03a320ed718e28fe0c541009cc63
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-16 13:01:50 -0500

    sends tokens, but only queries array of users instead of actual mongodb

---
+++ db/users.js
@@ -0,0 +1,11 @@
+var users = [{
+    id: 1,
+    email: "john@mail.com",
+    password: "john123"
+}, {
+    id: 2,
+    email: "sarah@mail.com",
+    password: "sarah123"
+}];
+
+module.exports = users;
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// user authentication modified from https://blog.jscrambler.com/implementing-jwt-using-passport/
+
 const express = require('express')
 const parser = require('body-parser')
+var jwt = require("jwt-simple");  
 const cors = require('cors')
 const passport = require('passport')
+var auth = require("./auth.js")();
+var cfg = require("./config.js");
+var users = require("./db/users.js");
 
 const Home = require('./db/schema').Home
 const User = require('./db/schema').User
@@ -10,11 +16,42 @@ const FollowedHomes = require('./db/schema').FollowedHomes
 const app = express()
 
 app.use(parser.json())
+app.use(auth.initialize());
+
 app.use(cors())
 
 app.get('/', (req, res) => {
 	res.send('hello world!')
 })
+
+// sends back the user's info.  This private route will only run for authenticated token and you can use the req.user.id object inside this route, because this data will be available if you send the right token
+app.get("/user", auth.authenticate(), function(req, res) {
+    res.json(users[req.user.id]);
+});
+
+//This route will be responsible for generating an encoded token with a payload, given to the user that sends the right e-mail and password via req.body.email and req.body.password in the request.
+app.post("/token", function(req, res) {
+    if (req.body.email && req.body.password) {
+        var email = req.body.email;
+        var password = req.body.password;
+        var user = users.find(function(u) {
+            return u.email === email && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, cfg.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+});
 
 app.get('/api/homes', (req, res) => {
 	Home.find()
