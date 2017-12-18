99284eab550fb5a3130c8cbcc3d12355dc6ae0b3
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-18 14:24:02 -0500

    deployed to mlab, set up heroku backend

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/home', { userMongoClient: true })
 
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/home', { userMongoClient: true });
+}
+
 mongoose.Promise = Promise
 
 module.exports = mongoose
